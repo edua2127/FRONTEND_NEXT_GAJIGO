9e3011e1c72751e826c56658ab18ce421e92f135
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2022-12-05 21:10:04 -0300

    feat: switch cadastroEvents to use RTK Query

--- src/pages/auth/cadastroEvents.tsx
+++ src/pages/auth/cadastroEvents.tsx
@@ -1,67 +1,39 @@
 import type { NextPage } from 'next'
-import React from 'react'
+import React, { useEffect } from 'react'
 import style from '@/styles/CadastroEvents.module.css'
-import UserService from '@/services/user.service'
-import EventService from '@/services/event.service'
-import { Event } from '@/types/event.types'
 import AppLayout from '@/layout/AppLayout'
+import { useCreateEventMutation } from '@/store/events/api'
+import { useGetCurrentUserQuery } from '@/store/auth/api'
 
 const CadastroEvents: NextPage = () => {
   const [name, setName] = React.useState('')
   const [description, setDescription] = React.useState('')
-  const [dateDeInicio, setDateDeInicio] = React.useState('')
-  const [dateDeFim, setDateDeFim] = React.useState('')
-  const [modoDeAtendimento, setModoDeAtendimento] = React.useState('')
-  const [statusDoEvento, setStatusDoEvento] = React.useState('')
+  const [startDate, setStartDate] = React.useState('')
+  const [endDate, setEndDate] = React.useState('')
+  const [attendanceMode, setAttendanceMode] = React.useState('')
+  const [status, setStatus] = React.useState('')
   const [owner, setOwner] = React.useState('')
   const [location, setLocation] = React.useState('')
 
-  function cadastroEvento() {
-    const evento = criaEvento()
-    console.log(evento)
-    EventService.create(evento)
-      .then(() => {
-        console.log('Evento criado com sucesso')
-        console.log(evento)
-      })
-      .catch((error) => {
-        console.log('Erro ao criar evento: ' + error)
-      })
-  }
+  const [saveEvent] = useCreateEventMutation()
+  const { data: currentUser } = useGetCurrentUserQuery()
 
-  function criaEvento() {
-    const evento: Event = {
-      name: '',
-      description: '',
-      interval: {
-        startDate: '',
-        endDate: '',
-      },
-      attendanceMode: '',
-      status: '',
-      owner: '',
-      location: '',
-    }
+  const criaEvento = () => ({
+    name,
+    description,
+    interval: {
+      startDate,
+      endDate,
+    },
+    attendanceMode,
+    status,
+    owner,
+    location,
+  })
 
-    evento.name = name
-    evento.description = description
-    evento.interval.startDate = dateDeInicio
-    evento.interval.endDate = dateDeFim
-    evento.attendanceMode = modoDeAtendimento
-    evento.status = statusDoEvento
-    evento.owner = owner
-    evento.location = location
-    return evento
-  }
-
-  async function transformaIdDoCorrentUserEmUrl() {
-    const user = await UserService.getCorrentUser()
-    setOwner('http://localhost:8080/api/users/' + user.id)
-  }
-
-  React.useEffect(() => {
-    transformaIdDoCorrentUserEmUrl()
-  }, [])
+  useEffect(() => {
+    setOwner('/' + currentUser?.id)
+  }, [currentUser])
 
   return (
     <AppLayout title='Cadastro de Eventos'>
@@ -90,8 +62,8 @@ const CadastroEvents: NextPage = () => {
             <label className={style.cadastro_events_label}>
               <span>Data de Início</span>
               <input
-                value={dateDeInicio.toString()}
-                onChange={(e) => setDateDeInicio(e.target.value)}
+                value={startDate.toString()}
+                onChange={(e) => setStartDate(e.target.value)}
                 type='datetime-local'
                 className={style.cadastro_events_input}
               />
@@ -99,8 +71,8 @@ const CadastroEvents: NextPage = () => {
             <label className={style.cadastro_events_label}>
               <span>Data de Fim</span>
               <input
-                value={dateDeFim.toString()}
-                onChange={(e) => setDateDeFim(e.target.value)}
+                value={endDate.toString()}
+                onChange={(e) => setEndDate(e.target.value)}
                 type='datetime-local'
                 className={style.cadastro_events_input}
               />
@@ -110,8 +82,8 @@ const CadastroEvents: NextPage = () => {
             <label className={style.cadastro_events_label}>
               <span>Modo de Atendimento</span>
               <select
-                value={modoDeAtendimento}
-                onChange={(e) => setModoDeAtendimento(e.target.value)}
+                value={attendanceMode}
+                onChange={(e) => setAttendanceMode(e.target.value)}
                 className={style.cadastro_events_input}
               >
                 <option value=''>Selecione</option>
@@ -123,8 +95,8 @@ const CadastroEvents: NextPage = () => {
             <label className={style.cadastro_events_label}>
               <span>Status do Evento</span>
               <select
-                value={statusDoEvento}
-                onChange={(e) => setStatusDoEvento(e.target.value)}
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
                 className={style.cadastro_events_input}
               >
                 <option value=''>Selecione</option>
@@ -136,7 +108,7 @@ const CadastroEvents: NextPage = () => {
             </label>
           </article>
           <article className={style.cadastro_events_article}>
-            {modoDeAtendimento === 'Offline' || modoDeAtendimento === 'Mixed' ? (
+            {attendanceMode === 'Offline' || attendanceMode === 'Mixed' ? (
               <label className={style.cadastro_events_label}>
                 <span>Localização</span>
                 <input
@@ -160,7 +132,7 @@ const CadastroEvents: NextPage = () => {
             )}
           </article>
           <article className={style.cadastro_events_article_button}>
-            <button onClick={cadastroEvento} className={style.btn_grad}>
+            <button onClick={() => saveEvent(criaEvento())} className={style.btn_grad}>
               Cadastrar
             </button>
           </article>
