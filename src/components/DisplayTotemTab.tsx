7bcdd35deb4f6bc769c7703d588a00072ecfb74f
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2022-12-08 09:17:22 -0300

    fix: change DisplayTotemTab to not be default

--- src/components/DisplayTotemTab.tsx
+++ src/components/DisplayTotemTab.tsx
@@ -1,24 +1,30 @@
-import { LinkTab } from "@/utils/LinkTab"
-import { useRouter } from "next/router"
+import { LinkTab } from '@/utils/LinkTab'
+import { useRouter } from 'next/router'
 import Tabs from '@mui/material/Tabs'
 
 type DisplayTotemTabProps = {
-    lectureId: string | string[] | undefined
-    defaultTab: number
+  lectureId: string | string[] | undefined
+  defaultTab: number
 }
 
-export default function DisplayTotemTab({ lectureId, defaultTab }: DisplayTotemTabProps) {
-    const router = useRouter()
-    const tabUrls = [`/display-totem/${lectureId}`, `/display-totem/${lectureId}/participants`]
+export function DisplayTotemTab({ lectureId, defaultTab }: DisplayTotemTabProps) {
+  const router = useRouter()
+  const tabUrls = [`/display-totem/${lectureId}`, `/display-totem/${lectureId}/participants`]
 
-    const handleTab = (_: React.SyntheticEvent, newValue: number) => {
-      router.push(tabUrls[newValue])
-    }
-  
-    return (
-        <Tabs value={defaultTab} onChange={handleTab} variant="fullWidth" style={{ width: '100%', justifySelf: 'flex-start'}} centered>
-            <LinkTab label='TOTEM' href={tabUrls[0]} />
-            <LinkTab label='PARTICIPANTES' href={tabUrls[1]} />
-        </Tabs>
-    )
+  const handleTab = (_: React.SyntheticEvent, newValue: number) => {
+    router.push(tabUrls[newValue])
+  }
+
+  return (
+    <Tabs
+      value={defaultTab}
+      onChange={handleTab}
+      variant='fullWidth'
+      style={{ width: '100%', justifySelf: 'flex-start' }}
+      centered
+    >
+      <LinkTab label='TOTEM' href={tabUrls[0]} />
+      <LinkTab label='PARTICIPANTES' href={tabUrls[1]} />
+    </Tabs>
+  )
 }
