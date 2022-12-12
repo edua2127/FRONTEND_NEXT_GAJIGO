60ffcca6742b95ba871da033fe2bd9d7a8fc1e18
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2022-12-12 09:40:39 -0300

    fix: authorization persistence

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,14 +1,15 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/dist/query'
 import { createApi } from '@reduxjs/toolkit/dist/query/react'
-import { RootState } from '.'
+import Cookie from 'universal-cookie'
 
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/`,
     credentials: 'same-origin',
     mode: 'cors',
-    prepareHeaders: (headers, { getState }) => {
-      const token = (getState() as RootState).auth.token
+    prepareHeaders: (headers) => {
+      const cookies = new Cookie()
+      const token = cookies.get('token')
 
       if (token) {
         headers.set('Authorization', `Bearer ${token.access_token}`)
