34007a199fe1025c984c1eb31c2b59de3bff45b9
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2022-12-13 09:15:10 -0300

    fix: upgrade insecure requests

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -13,6 +13,7 @@ export default class MyDocument extends Document {
             rel='stylesheet'
             href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
           />
+          <meta http-equiv='Content-Security-Policy' content='upgrade-insecure-requests' />
         </Head>
         <body>
           <Main />
